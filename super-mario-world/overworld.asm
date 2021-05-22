57ee605c39e7122760404fe9edc24cd5e2a08d67
Vitor Vilela <vitor.vilela.mario@gmail.com>
2021-05-22 14:20:09 -0300

    Update .gitignore

--- super-mario-world/overworld.asm
+++ super-mario-world/overworld.asm
@@ -1,3 +1,6 @@
 ; Dedicated widescreen overworld patch
 ; Special thanks to Medic for the forcetask done together in https://www.smwcentral.net/?p=section&a=details&id=12773
 
+; overworld maximum horizontal scrolling range
+org $048221
+dw $0000,$0101,$0000,$0101
