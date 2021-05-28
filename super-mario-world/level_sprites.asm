2639de93b9a6109972f943526f37890efd822965
Vitor Vilela <vitor.vilela.mario@gmail.com>
2021-05-27 23:15:34 -0300

    sprite TO DO list

--- super-mario-world/level_sprites.asm
+++ super-mario-world/level_sprites.asm
@@ -1,3 +1,13 @@
 ; Dedicated code for improving level sprites
 ; For widescreen SMW support
 
+; TO DO: cluster sprites
+; TO DO: regular sprites
+; TO DO: bounce sprites
+; TO DO: quake sprites
+; TO DO: extended sprites
+; TO DO: shooter sprites
+; TO DO: smoke sprites
+; TO DO: spinnning coin sprites
+; TO DO: minor extended sprites
+; TO DO: generator sprites (adjust spawn position)
