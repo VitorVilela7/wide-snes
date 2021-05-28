7b04df74b656f600e6fd1446940f936750b2096d
Vitor Vilela <vitor.vilela.mario@gmail.com>
2021-05-28 01:31:15 -0300

    Initial spinning coin support

--- super-mario-world/level_sprites.asm
+++ super-mario-world/level_sprites.asm
@@ -8,6 +8,106 @@
 ; TO DO: extended sprites
 ; TO DO: shooter sprites
 ; TO DO: smoke sprites
-; TO DO: spinnning coin sprites
+; TO DO: spinnning coin sprites (from ? block)
 ; TO DO: minor extended sprites
 ; TO DO: generator sprites (adjust spawn position)
+
+; TO DO: add koopaling hair fix
+; TO DO: add "S" from MARIO START
+; TO DO: add Luigi graphics
+
+;- RAM addresses definitions
+;===========================
+
+; x position high byte for smoke sprites.
+!smoke_x_high = $18C5
+
+;- Spinning coins
+;================
+
+pushpc
+	org $0299E3
+		spinning_despawn:
+			; make sure AXY is 8-bit
+			SEP #$30
+			STZ $17D0,x
+			RTS
+
+		warnpc $0299E9
+		
+	org $029A6D
+		spinning_return:
+		
+	org $029A18
+		; optimize NES legacy to SNES
+		LDA $17D4,x
+		CMP $02
+		LDA $17E8,x
+		SBC $001D,y
+		BNE spinning_return
+	
+		; 16-bit calculation
+		JSL spinning_wide
+		
+		; widescreen bounds
+		CMP.w #$0000-!extra_columns
+		BMI spinning_despawn
+		CMP.w #$0100+!extra_columns
+		BPL spinning_despawn
+		
+		SEP #$20
+	
+	; print pc
+	warnpc $029A35
+	
+	org $029A5C
+		JSL spinning_load_high
+		
+	org $029A7A
+		JSL spinning_add4
+		STA $0E
+	
+	; print pc
+	warnpc $029A80
+	
+	org $029A9E
+		LDA $0E
+
+pullpc
+
+spinning_wide:
+	LDA $17E0,x
+	SEC
+	SBC $03
+	STA $00
+
+	LDA $17EC,x
+	SBC $001B,y
+	STA $0E
+	XBA
+	LDA $00
+
+	REP #$20
+	RTL
+
+spinning_load_high:
+	; load MSB x position + tile size
+	LSR
+	TAY
+	LDA $0E
+	AND #$01
+	ORA #$02
+	RTL
+	
+spinning_add4:
+	STA $0200,y
+	STA $0204,y
+	LDA $0E
+	ADC #$00
+	AND #$01 ;mask it already for the MSB bit
+	RTL
+
+; TO DO for spinning: smoke sprites at 029AE5
+; TO DO for spinning: score [10pts] sprite support
+
+	
