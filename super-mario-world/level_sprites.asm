a95406bf2b47fca45d158172f2f4bc0e13204b6f
Vitor Vilela <vitor.vilela.mario@gmail.com>
2021-05-30 00:41:25 -0300

    Add some minor extended sprites

--- super-mario-world/level_sprites.asm
+++ super-mario-world/level_sprites.asm
@@ -298,4 +298,215 @@ pushpc
 	org $0286D6
 		STA $16D5,y
 
-pullpc	
+pullpc
+
+;- Minor extended sprites
+;========================
+
+pushpc
+	; hatch yoshi egg animatino
+	org $01F7CD
+		JSL minor_load_spr_x
+		
+	org $01F7FF
+		JSL minor_store_spr_x
+		NOP
+
+pullpc
+
+minor_load_spr_x:
+	; restore
+	LDA $E4,x
+	STA $00
+	
+	; load sprite x position high
+	LDA $14E0,x
+	STA $01
+	RTL
+	
+minor_store_spr_x:
+	; restore
+	STA $1808,x
+	
+	; put logic on sprite high position
+	LDA $01
+	ADC #$00
+	STA $18EA,x
+	
+	; restore
+	LDA $02
+	RTL
+	
+pushpc
+	; star power sparkles
+	org $0285B6
+		JSL minor_load_spr_x_star
+		
+	org $0285DB
+		JML minor_store_spr_x_star
+
+pullpc
+
+minor_load_spr_x_star:
+	ADC $94
+	STA $02
+	
+	LDA $95
+	ADC #$00
+	STA $03
+	
+	RTL
+	
+minor_store_spr_x_star:
+	; restore
+	STA $1850,y
+	
+	LDA $03
+	STA $18EA,y
+	
+	; end/restore
+	RTL
+	
+pushpc
+	; unused yoshi smoke
+	org $028C79
+		JSL minor_x_check
+		NOP
+		
+	org $028CB2
+		LDA $0F
+		
+	; boo stream
+	org $028D0A
+		JSL minor_x_check
+		NOP
+		
+	org $028D3C
+		LDA $0E
+		
+	; water splash
+	org $028D4F
+		JSL minor_x_calc_check
+		
+		BRA +
+		NOP #4
+	+
+	
+	; print pc
+	warnpc $028D59
+	
+	; water splash
+	org $028D97
+		JSL minor_water_x_correction
+		
+	org $028DCA
+		LDA $0E
+		
+	; rip van fish 'zzz'
+	org $028E23
+		JSL minor_x_calc_check
+		BEQ +
+		
+		BRA rip_van_fish_erase
+		NOP #4
+	
+	+	LDA $00
+		
+	;print pc
+	warnpc $028E31
+	
+	org $028E6D
+		LDA $0F
+		
+	org $028E76
+		rip_van_fish_erase:
+		
+	org $028E4B
+		JML abort_rip_van_fish_if_timeout
+		
+	org $02C10E
+		JSL spawn_rip_van_fish_zs
+		NOP
+		
+	; ignore horizontal offscreen flag (standard sprite)
+	; for rip van fish ($15A0+x)
+	org $02C0DC
+		LDA #$00
+		NOP
+pullpc
+		
+
+minor_x_check:
+	LDA $18EA,x
+	SBC $1B
+	STA $01
+	
+	REP #$20
+	LDA $00
+	CMP.w #$0000-!extra_columns-$0020
+	BMI .return
+	CMP.w #$0100+!extra_columns+$0020
+	BMI .ok
+	
+.return
+	SEP #$20
+	LDA #$01 ; FAIL
+	RTL
+
+.ok
+	SEP #$20
+	LDA $01
+	AND #$01
+	STA $0F
+	ORA #$02
+	STA $0E
+	
+	LDA #$00 ; OK
+	RTL
+	
+minor_x_calc_check:
+	LDA $1808,x
+	SEC
+	SBC $1A
+	STA $00
+	
+	BRA minor_x_check
+	
+minor_water_x_correction:
+	STA $00
+	
+	LDA $18EA,x
+	SBC $1B
+	AND #$01
+	ORA #$02
+	STA $0E
+	
+	LDA $00
+	RTL
+	
+spawn_rip_van_fish_zs:
+	ADC #$06
+	STA $1808,y
+	
+	LDA $14E0,x
+	ADC #$00
+	STA $18EA,y
+	RTL
+
+abort_rip_van_fish_if_timeout:
+	CMP #$14
+	BEQ .undraw
+	JML $028E4F|!bank
+	
+.undraw
+	; rip van fish 'z' if lifespan timeouts,
+	; it removes itself without setting $0420...
+	TYA
+	LSR
+	LSR
+	TAY
+	; at least set the tile msb
+	LDA $0F
+	STA $0420,y
+
+	JML $028E76|!bank
