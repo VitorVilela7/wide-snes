bbb3a75ce8d5f1fef24549312d2ac42849f31ec2
Vitor Vilela <vitor.vilela.mario@gmail.com>
2021-05-31 00:54:50 -0300

    Add generators support

--- super-mario-world/level_sprites.asm
+++ super-mario-world/level_sprites.asm
@@ -11,7 +11,6 @@
 
 ; TO DO: cluster sprites
 ; TO DO: regular sprites
-; TO DO: generator sprites (adjust spawn position)
 
 ; TO DO: check bounce sprites on vertical levels. $02925C
 ; TO DO: add koopaling hair fix
@@ -20,6 +19,7 @@
 ; TO DO: test more carefully yoshi eggs on screen edges. (fix needed)
 ; TO DO: fix torpedo ted's hand on widescreen area.
 ; TO DO: fix jump 'strings'
+; TO DO: fix 'smashed' OAM on koopas without shell.
 
 ; DONE: smoke sprites
 ; DONE: spinnning coin sprites (from ? block)
@@ -30,6 +30,7 @@
 ; DONE: minor extended sprites
 ; DONE: shooter sprites
 ; DONE: extended sprites
+; DONE: generator sprites (adjust spawn position)
 
 ; DONE for spinning: glitter effect (using smoke sprites as proxy).
 ; DONE for spinning: score [10pts] sprite support
@@ -1018,3 +1019,86 @@ bullet_bill_shooter:
 
 	SEP #$20
 	JML $02B4A1|!bank
+
+;- Sprite Generators
+;===================
+
+; Eeries
+pushpc
+	org $02B2D0
+		db $F0-!extra_columns
+		db $FF+!extra_columns
+pullpc
+
+; Para goomba, bob-omb, etc,
+; Ignored.
+
+; Dolphins
+
+pushpc
+	org $02B25E
+		db $10+!extra_columns
+		db $E0-!extra_columns
+pullpc
+
+; Fish Generator: ignored;
+; because the range is already tight.
+; and couldn't find much benefit.
+
+; Super koopa, bubble and bullet bill generator
+pushpc
+	org $02B1B8
+		db $E0-!extra_columns
+		db $10+!extra_columns
+
+pullpc
+
+; Multiple bullet generator
+; the -$10 and +$10 aren't needed, these are personal improvements.
+pushpc
+	org $02B135
+		JSL generator_high_table
+		
+	org $02B0FA
+		; surrounded
+		db $00-!extra_columns-$10
+		db $00-!extra_columns-$10
+		db $40
+		db $C0
+		db $F0+!extra_columns+$10
+		
+		; diagonal
+		db $00-!extra_columns
+		db $00-!extra_columns
+		db $F0+!extra_columns
+		db $F0+!extra_columns
+		
+pullpc
+
+generator_high_table:
+	LDA $1B
+	ADC.l multi_bullet_high_table,x
+	RTL
+
+multi_bullet_high_table:
+	; surrounded
+	db $FF
+	db $FF
+	db $00
+	db $00
+	db $01
+	
+	; diagonal
+	db $FF
+	db $FF
+	db $01
+	db $01
+	
+; Bowser fire
+pushpc
+	org $02B06D
+		ADC.b #$FF+!extra_columns
+		
+	org $02B073
+		ADC.b #$01
+pullpc
